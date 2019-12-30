b4bd92fd9e60cc337de941ad7740dbbcb416677d
Robert Freeman <zarohian@yahoo.com>
2019-12-29 21:09:23 -0500

    add script to take a ss every second and add all to disc afterwards

---
+++ recordScreenshots.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import ImageGrab
+from PIL import Image
+import time
+import keyboard
+import winsound
+
+pils = []
+path = 'C:/Users/Robbie/Documents/GitHub/clipd-backend/data'
+while True:
+    print("work")
+    time.sleep(1.0)
+    print("?")
+    printscreen_pil =  ImageGrab.grab()
+    pils.append(printscreen_pil)
+    winsound.Beep(1000, 100)
+    if keyboard.is_pressed('esc'):
+        break
+n = 0
+print("saving images to disc")
+for i in pils:
+    i.save(path + '/testScreen' + str(n) + '.png', "PNG")
+    n = n + 1
